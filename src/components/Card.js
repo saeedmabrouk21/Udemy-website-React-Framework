530cc1d01c510afbbcb0466168cb8a2b809c3787
saeedmabrouk21 <saeedmabrouk21@gmail.com>
2022-08-25 15:50:32 +0200

    perform some modifications based on review notes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./Card.module.css";
 
 function adjustStars(rating) {
-  const fullStar = (
+  const FullStar = () => (
     <span className={[styles.checked, "fa fa-star"].join(" ")}></span>
   );
-  const halfStar = (
+  const HalfStar = (
     <span className={[styles.checked, "fa fa-star-half-full"].join(" ")}></span>
   );
-  const noStar = (
+  const NoStar = () => (
     <span className={[styles.checked, "fa fa-star-o"].join(" ")}></span>
   );
 
@@ -17,40 +17,30 @@ function adjustStars(rating) {
   let remainder = rating - whole;
   return (
     <>
-      {--whole >= 0 ? fullStar : ""}
-      {--whole >= 0 ? fullStar : ""}
-      {--whole >= 0 ? fullStar : ""}
-      {--whole >= 0 ? fullStar : ""}
-      {--whole >= 0 ? fullStar : ""}
-      {remainder >= 0.3 && remainder < 0.8
-        ? halfStar
-        : remainder < 0.3
-        ? noStar
-        : fullStar}
-      {--emptyStars >= 0 ? noStar : ""}
-      {--emptyStars >= 0 ? noStar : ""}
-      {--emptyStars >= 0 ? noStar : ""}
-      {--emptyStars >= 0 ? noStar : ""}
+    {[...Array(whole)].map((_,ix) => <FullStar key={ix} />)}
+  {remainder > 0 && remainder < 0.3? <NoStar /> : remainder < 0.8 ? HalfStar : <FullStar />} 
+  {[...Array(emptyStars)].map((_,ix) => <NoStar key={ix} />)}
+      
     </>
   );
 
 }
 
-function Card(obj) {
+function Card({json}) {
     return (
     <article>
       <a className={styles.course} href={"#"}>
-        <img src={obj.json.image} alt="pyhon" />
-        <h3>{obj.json.title}</h3>
+        <img src={json.image} alt="pyhon" />
+        <h3>{json.title}</h3>
         <h4 className={styles.grey}>
-          {obj.json.instructors.map((y) => y["name"]).toString()}
+          {json.instructors.map((y) => y["name"]).toString()}
         </h4>
         <div>
-          <span className={styles.rating}>{obj.json.rating.toFixed(2)}</span>
-          {adjustStars(obj.json.rating.toFixed(2))}
-          <span className={styles.grey}>({obj.json.people})</span>
+          <span className={styles.rating}>{json.rating.toFixed(2)}</span>
+          {adjustStars(json.rating.toFixed(2))}
+          <span className={styles.grey}>({json.people})</span>
         </div>
-        <h5>E£{obj.json.price}</h5>
+        <h5>E£{json.price}</h5>
       </a>
     </article>
   );
